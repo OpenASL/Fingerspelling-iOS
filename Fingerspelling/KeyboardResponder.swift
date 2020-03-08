25fb268466055596380e9d52fa87a54e8f1f3c24
Steven Loria <sloria1@gmail.com>
2020-03-08 12:44:51 -0400

    Run pre-commit run --all-files

--- Fingerspelling/KeyboardResponder.swift
+++ Fingerspelling/KeyboardResponder.swift
@@ -9,26 +9,26 @@ import SwiftUI
 
 // https://stackoverflow.com/questions/56491881/move-textfield-up-when-thekeyboard-has-appeared-by-using-swiftui-ios
 final class KeyboardResponder: ObservableObject {
-    private var notificationCenter: NotificationCenter
-    @Published private(set) var currentHeight: CGFloat = 0
+  private var notificationCenter: NotificationCenter
+  @Published private(set) var currentHeight: CGFloat = 0
 
-    init(center: NotificationCenter = .default) {
-        notificationCenter = center
-        notificationCenter.addObserver(self, selector: #selector(keyBoardWillShow(notification:)), name: UIResponder.keyboardWillShowNotification, object: nil)
-        notificationCenter.addObserver(self, selector: #selector(keyBoardWillHide(notification:)), name: UIResponder.keyboardWillHideNotification, object: nil)
-    }
+  init(center: NotificationCenter = .default) {
+    self.notificationCenter = center
+    self.notificationCenter.addObserver(self, selector: #selector(self.keyBoardWillShow(notification:)), name: UIResponder.keyboardWillShowNotification, object: nil)
+    self.notificationCenter.addObserver(self, selector: #selector(self.keyBoardWillHide(notification:)), name: UIResponder.keyboardWillHideNotification, object: nil)
+  }
 
-    deinit {
-        notificationCenter.removeObserver(self)
-    }
+  deinit {
+    notificationCenter.removeObserver(self)
+  }
 
-    @objc func keyBoardWillShow(notification: Notification) {
-        if let keyboardSize = (notification.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue)?.cgRectValue {
-            currentHeight = keyboardSize.height
-        }
+  @objc func keyBoardWillShow(notification: Notification) {
+    if let keyboardSize = (notification.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue)?.cgRectValue {
+      self.currentHeight = keyboardSize.height
     }
+  }
 
-    @objc func keyBoardWillHide(notification: Notification) {
-        currentHeight = 0
-    }
+  @objc func keyBoardWillHide(notification _: Notification) {
+    self.currentHeight = 0
+  }
 }
--- FingerspellingTests/FingerspellingTests.swift
+++ FingerspellingTests/FingerspellingTests.swift
@@ -5,29 +5,27 @@
 //  Created by Steven Loria on 3/7/20.
 //
 
-import XCTest
 @testable import Fingerspelling
+import XCTest
 
 class FingerspellingTests: XCTestCase {
+  override func setUp() {
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+  }
 
-    override func setUp() {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+  override func tearDown() {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+  }
+
+  func testExample() {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+  }
+
+  func testPerformanceExample() {
+    // This is an example of a performance test case.
+    measure {
+      // Put the code you want to measure the time of here.
     }
-
-    override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
-    }
-
+  }
 }
