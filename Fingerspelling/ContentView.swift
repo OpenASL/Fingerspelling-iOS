40359f30fa67454caf53ceff9ec108deaed665d6
Lauren Barker <ms.lebarker@gmail.com>
2020-03-08 12:03:35 -0400

    Add text input with hardcoded validation (#1)
    * Add text input with hardcoded validation
    
    This adds an input field with a submit button. On submit the input is
    validated against a hardcoded value and the border color of the input
    field is updated. Currently this is pretty dumb and just shows different
    versions of the input field based on state.
    
    This also adds KeyboardResponder which return the height of the
    keyboard. It's a simple way to ensure the input is showing but it
    doesn't adjust for where the input is on the screen.
    
    * Update project settings (add KeyboardResponder and necessary stuff for running on my phone)
    
    * Minor refactoring
    
    Co-authored-by: Steven Loria <sloria1@gmail.com>

--- Fingerspelling/ContentView.swift
+++ Fingerspelling/ContentView.swift
@@ -8,19 +8,41 @@
 import SwiftUI
 
 struct ContentView: View {
-  @State var alertIsVisible: Bool = false
-  @State var speed = 5.0
-  let minSpeed = 0.0
-  let maxSpeed = 10.0
+  @State private var alertIsVisible: Bool = false
+  @State private var speed = 5.0
+  @State private var answer: String = ""
+  @State private var showValidation: Bool = false
+  @ObservedObject private var keyboard = KeyboardResponder()
+  
+  private let minSpeed = 0.0
+  private let maxSpeed = 10.0
+  
+  private var isAnswerValid: Bool {
+    let trimmedString = self.answer.trimmingCharacters(in: .whitespaces).lowercased()
+    return trimmedString == "test"
+  }
 
   var body: some View {
     VStack {
       Spacer()
       HStack {
         Image("B").resizable().aspectRatio(contentMode: .fit)
-      }
-      .padding(.horizontal, 100)
-      Spacer()
+      }.padding(.horizontal, 100)
+
+      HStack {
+        if self.showValidation {
+          TextField("Answer", text: $answer).border(self.isAnswerValid ? Color.green : Color.red)
+        } else {
+          TextField("Answer", text: $answer)
+        }
+        Spacer()
+        Button(action: {
+          self.showValidation = true
+        }) {
+          Text("Check")
+        }
+      }.padding(.top, 20)
+      
       VStack {
         HStack {
           Text("Slow")
@@ -32,13 +54,13 @@ struct ContentView: View {
         }) {
           Text("Reset speed")
         }
-      }
-      .padding(.horizontal, 40)
-      .padding(.vertical, 30)
+      }.padding(.vertical, 30)
       
       HStack {
         Button(action: {
           self.alertIsVisible = true
+          self.showValidation = false
+          self.answer = ""
         }) {
           Text("Next word")
         }
@@ -49,8 +71,12 @@ struct ContentView: View {
           )
         }
       }
-      .padding(.bottom, 20)
     }
+    .padding(.vertical, 20)
+    .padding(.horizontal, 40)
+    // Move the current UI up when the keyboard is active
+    .padding(.bottom, keyboard.currentHeight)
+    .animation(.easeIn(duration: 0.16))
   }
 }
 
