f27b3c05f8785f06747d3f604468bb9129316ab8
Steven Loria <sloria1@gmail.com>
2020-03-15 13:40:50 -0400

    DRY settings pickers

--- Fingerspelling/ContentView.swift
+++ Fingerspelling/ContentView.swift
@@ -319,27 +319,37 @@ struct GameSettings: View {
     }
   }
 
+  struct LabeledPicker<SelectionValue: Hashable, Content: View>: View {
+    var selection: Binding<SelectionValue>
+    var label: String
+    var content: () -> Content
+
+    var body: some View {
+      Section(header: Text(self.label.uppercased())) {
+        Picker(selection: self.selection, label: Text(self.label)) {
+          self.content()
+        }.pickerStyle(SegmentedPickerStyle())
+      }
+    }
+  }
+
   var body: some View {
     NavigationView {
       Form {
         // MARK: Max word length
 
-        Section(header: Text("Max word length".uppercased())) {
-          Picker(selection: self.$settings.maxWordLength, label: Text("Max word length")) {
-            ForEach(Self.wordLengths, id: \.self) {
-              Text($0 == Int.max ? "Any" : "\($0) letters").tag($0)
-            }
-          }.pickerStyle(SegmentedPickerStyle())
+        LabeledPicker(selection: self.$settings.maxWordLength, label: "Max word length") {
+          ForEach(Self.wordLengths, id: \.self) {
+            Text($0 == Int.max ? "Any" : "\($0) letters").tag($0)
+          }
         }
 
         // MARK: Game Mode
 
-        Section(header: Text("Mode".uppercased())) {
-          Picker(selection: self.$settings.gameMode, label: Text("Mode")) {
-            ForEach(GameMode.allCases, id: \.self) {
-              Text($0.rawValue).tag($0.rawValue)
-            }
-          }.pickerStyle(SegmentedPickerStyle())
+        LabeledPicker(selection: self.$settings.gameMode, label: "Mode") {
+          ForEach(GameMode.allCases, id: \.self) {
+            Text($0.rawValue).tag($0.rawValue)
+          }
         }
       }
       .navigationBarTitle(Text("Settings"), displayMode: .inline)
