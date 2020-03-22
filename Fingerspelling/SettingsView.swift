d1659e11d98fab9025a0e096d4e4b7f9add7bd4f
Steven Loria <sloria1@gmail.com>
2020-03-22 15:51:07 -0400

    Add feedback and rating button

--- Fingerspelling/SettingsView.swift
+++ Fingerspelling/SettingsView.swift
@@ -1,11 +1,14 @@
 import SwiftUI
 
 struct SettingsView: View {
+  @State var isShowingFeedbackAlert = false
   @EnvironmentObject private var settings: UserSettings
   @EnvironmentObject private var game: GameState
   @EnvironmentObject private var playback: PlaybackService
 
   static let wordLengths = Array(3 ... 6) + [Int.max]
+  static let feedbackEmail = "sloria1+Fingerspelling@gmail.com"
+  static let appId = "id1503242863"
 
   struct LabeledPicker<SelectionValue: Hashable, Content: View>: View {
     var selection: Binding<SelectionValue>
@@ -31,15 +34,42 @@ struct SettingsView: View {
         }
 
         Section {
+          Button(action: { self.isShowingFeedbackAlert.toggle() }) {
+            Text("Send Feedback").foregroundColor(.primary).fontWeight(.semibold)
+          }
+          Button(action: self.handleRate) {
+            Text("Rate Fingerspelling").foregroundColor(.primary)
+          }
           NavigationLink(destination: AboutView()) { Text("About") }
         }
       }
       .navigationBarTitle("Settings")
       .navigationBarItems(trailing: Button(action: self.handleToggleSettings) { Text("Done") })
     }
+    .alert(isPresented: self.$isShowingFeedbackAlert) {
+      Alert(
+        title: Text("Send Feedback"),
+        message: Text("Send your feedback, questions, and ideas to \(Self.feedbackEmail)"),
+        primaryButton: .default(Text("Open Mail").fontWeight(.bold), action: self.handleSendFeedback),
+        secondaryButton: .default(Text("Not Now"))
+      )
+    }
   }
 
-  func handleToggleSettings() {
+  private func handleSendFeedback() {
+    if let url = URL(string: "mailto:\(Self.feedbackEmail)") {
+      UIApplication.shared.open(url)
+    }
+  }
+
+  private func handleRate() {
+    if let url = URL(string: "https://itunes.apple.com/us/app/appName/\(Self.appId)?mt=8&action=write-review"),
+      UIApplication.shared.canOpenURL(url) {
+      UIApplication.shared.open(url, options: [:], completionHandler: nil)
+    }
+  }
+
+  private func handleToggleSettings() {
     self.game.toggleSheet(.settings)
     self.playback.stop()
   }
