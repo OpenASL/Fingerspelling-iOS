4cf4adaa4937dd7af9b1ac3e94f186fdbd16bbac
Steven Loria <sloria1@gmail.com>
2020-03-25 00:43:57 -0400

    feat: regenerate word list; add blacklist

--- scripts/gen_words.py
+++ scripts/gen_words.py
@@ -1,9 +1,11 @@
 import json
-import urllib.request
 import pathlib
+import sqlite3
+import urllib.request
 
 HERE = pathlib.Path(__file__).parent
 OUTPUT = HERE.parent / "Fingerspelling" / "Data" / "Words.swift"
+BLACKLIST = HERE / "blacklist.db"
 
 WORDS_URL = "https://raw.githubusercontent.com/derekchuank/high-frequency-vocabulary/master/10k.txt"
 NAUGHTY_WORDS_URL = "https://raw.githubusercontent.com/sloria/List-of-Dirty-Naughty-Obscene-and-Otherwise-Bad-Words/en/en"
@@ -22,20 +24,24 @@ def fetch(url):
 
 
 def main():
+    con = sqlite3.connect("scripts/blacklist.db")
+
+    blacklisted_words = {
+        row[0] for row in con.execute("select word from blacklisted_words")
+    }
     print("Fetching words lists...")
     naughty_resp = fetch(NAUGHTY_WORDS_URL)
-    naughty_words = set()
     for line in naughty_resp.readlines():
         word = line.decode("utf-8").lower().strip()
         # very naive inflection, but fine for out purposes
         plural, past, gerund = f"{word}s", f"{word}ed", f"{word}ing"
-        naughty_words |= {word, plural, past, gerund}
+        blacklisted_words |= {word, plural, past, gerund}
 
     words_resp = fetch(WORDS_URL)
     words = []
     for line in words_resp.readlines():
         word = line.decode("utf-8").lower().strip()
-        if len(word) > 2 and word not in naughty_words:
+        if len(word) > 2 and word not in blacklisted_words:
             words.append(word)
 
     print(f"Writing to {OUTPUT}...")
